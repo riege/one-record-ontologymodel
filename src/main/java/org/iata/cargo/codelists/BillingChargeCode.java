1b3101273eda56a9adb4f11b2181e4674d4d5d99
Martin Skopp <skopp@riege.com>
2022-02-16 11:42:04 +0100

    Add BillingChargeCode enum for new Ratings#billingChargeCode

---
+++ src/main/java/org/iata/cargo/codelists/BillingChargeCode.java
@@ -0,0 +1,26 @@
+package org.iata.cargo.codelists;
+
+public enum BillingChargeCode {
+    CASS_INVOICE_AMOUNT("NI"),
+    CASS_NET_AMOUNT("CN"),
+    CHARGE_SUMMARY_TOTAL("CT"),
+    COMMISSION("CO"),
+    INSURANCE("IN"),
+    SALES_INCENTIVE("SI"),
+    TAXES("TX"),
+    TOTAL_OTHER_CHARGES_DUE_AGENT("OA"),
+    TOTAL_OTHER_CHARGES_DUE_CARRIER("OC"),
+    TOTAL_WEIGHT_CHARGE("WT"),
+    VALUATION_CHARGE("VC")
+    ;
+
+    private final String code;
+
+    private BillingChargeCode(String code) {
+        this.code = code;
+    }
+
+    public String code() {
+        return code;
+    }
+}
