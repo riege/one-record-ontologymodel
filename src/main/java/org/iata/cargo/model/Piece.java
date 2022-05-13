4d9eb1db4001429bab53ea40a1b8a3d6a2d902e1
Martin Skopp <skopp@riege.com>
2022-05-13 12:58:42 +0200

    Removal of mincardinality 1 for Piece#volumetricWeight Adopts https://github.com/IATA-Cargo/ONE-Record/commit/40571701beccc7726d0b1eba3ce9bec873b1f5e9

--- src/main/java/org/iata/cargo/model/Piece.java
+++ src/main/java/org/iata/cargo/model/Piece.java
@@ -219,7 +219,6 @@ public class Piece
      */
     @OWLObjectProperty(iri = Vocabulary.s_p_volumetricWeight)
     @ParticipationConstraints({
-        @ParticipationConstraint(owlObjectIRI = Vocabulary.s_c_Thing, min = 1, max = -1),
         @ParticipationConstraint(owlObjectIRI = Vocabulary.s_c_Thing, max = 1)
     })
     @JsonProperty(Vocabulary.s_p_volumetricWeight)
